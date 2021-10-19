df927124d5576a77d38f56a729311f24bf344815
Augustine Cobbold <augustinecobbold6@gmail.com>
2021-10-19 18:52:32 +0000

    added initials

---
+++ src/App.js
@@ -0,0 +1,57 @@
+
+import { useState, useEffect } from "react";
+import Weather from "./component/Weather";
+
+function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+
+    fetchData();
+
+    return () => {
+      setData([]);
+    };
+  }, [lat, long]);
+
+  return (
+    <div className="App">
+      {typeof data.main != "undefined" ? (
+        <Weather weatherData={data} />
+      ) : (
+        <div
+          style={{
+            fontSize: "50px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            color: "#fff",
+            background: "linear-gradient(to bottom, rgb(124, 102, 74),rgb(66, 69, 42))"
+          }}
+        >
+          <p>loading...</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Weather.js
@@ -0,0 +1,67 @@
+import moment from "moment";
+import "./style.css";
+
+const refresh = () => {
+  window.location.reload();
+};
+
+function Weather({ weatherData }) {
+  return (
+    <div className="weather-container">
+      <div className="weather--box">
+        <div className="reload--container">
+          <button onClick={refresh}>Reload <i className='fas fa-sync'></i></button>
+        </div>
+        <div className="city">
+          <p>{weatherData.name}</p>
+        </div>
+
+        <div className="temp">
+          <p>{weatherData.main.temp} &deg;c</p>
+        </div>
+
+        <div className="dash-container">
+          <div className="dash"></div>
+        </div>
+
+        <div className="sun">
+          <div className="sun-span">
+            <span className="material-icons">wb_sunny</span> <br />
+            {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString(
+              "en-IN"
+            )}
+          </div>
+          <div className="sun-span">
+            <span className="material-icons">brightness_medium</span> <br />
+            {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString(
+              "en-IN"
+            )}
+          </div>
+        </div>
+
+        <div className="des">
+          <span className="material-icons">cloud</span> <br />
+          <p>{weatherData.weather[0].description}</p>
+        </div>
+
+        <div className="humidity">
+          <p>
+            <span>Humidity</span> <br /> {weatherData.main.humidity} %
+          </p>
+        </div>
+
+        <div className="date">
+          <div>
+            <span>Day</span> <br /> {moment().format("dddd")}
+          </div>
+          <div>
+            <span>Date</span> <br />
+            {moment().format("LL")}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Weather;
